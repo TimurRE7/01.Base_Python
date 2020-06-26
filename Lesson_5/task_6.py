a0fbcb1ba4b30d76f10258129e2fdccffb1fd006
Timur <timurre7@gmail.com>
2020-06-26 15:58:41 +0300

    add task 6

--- Lesson_5/task_6.py
+++ Lesson_5/task_6.py
@@ -1,16 +1,14 @@
 my_dict = {}
 with open('task_6_file.txt', encoding='utf-8') as file:
     text = file.readlines()
-    print(text)
     for line in text:
         string = line.split()
-        print(string)
         summa = 0
-        for _ in line[1:]:
+        for _ in string[1:]:
             num = _.split('(')
-            print(num)
             try:
                 summa = summa + int(num[0])
             except ValueError:
                 pass
-        print(summa)
+        my_dict[string[0]] = summa
+print(my_dict)
