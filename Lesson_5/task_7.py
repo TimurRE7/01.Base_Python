98c5bf72f51e139ff329308279b29625f19ab435
Timur <timurre7@gmail.com>
2020-06-26 16:28:08 +0300

    add 7 task

--- Lesson_5/task_7.py
+++ Lesson_5/task_7.py
@@ -1,4 +1,23 @@
-line = ['Информатика:', '100(л)', '50(пр)', '20(лаб).']
-for x in line[1:]:
-    num = x.split('(')
-    print(num)
+import json
+
+my_dict = {}
+summary_profit = 0
+i = 0
+with open('task_7_file.txt', encoding='utf-8') as file:
+    task_list = file.readlines()
+    for line in task_list:
+        string = line.split()
+        if string[2] > string[3]:
+            summary_profit = summary_profit + int(string[2]) - int(string[3])
+            i +=1
+        my_dict[string[0]] = int(string[2]) - int(string[3])
+
+average_profit = summary_profit / i
+profit_dict = {'average_profit': average_profit}
+my_list = [my_dict, profit_dict]
+
+print(my_dict)
+print(my_list)
+
+with open('task_7.json', 'w') as f_json:
+    json.dump(my_list, f_json)
